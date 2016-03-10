44b9a44f010f65cfca5919a62738fef76e7a2e57
aaron <aaron@tuenti.com>
2016-03-10 18:58:57 +0100

    Adding sleep

--- countdown/core-pygame.py
+++ countdown/core-pygame.py
@@ -72,6 +72,7 @@ while not DONE:
     if IS_READY:
         draw(background, counter)
         counter.update()
+    time.sleep(0.5)
 
     screen.blit(background, (0, 0))
     pygame.display.flip()
